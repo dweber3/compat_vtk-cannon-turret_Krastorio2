c3b950f93c1d799fea63631dc87edc05f1c0ef6b
Daniel Weber <pennwe@gmail.com>
2023-07-25 14:31:14 -0400

    Set cannon-shell-magazine range equal to cannon-shell

---
+++ data-final-fixes.lua
@@ -0,0 +1,4 @@
+data.raw["ammo"]["cannon-shell-magazine"].ammo_type.action.action_delivery.max_range = data.raw["ammo"]["cannon-shell"].ammo_type.action.action_delivery.max_range
+data.raw["ammo"]["explosive-cannon-shell-magazine"].ammo_type.action.action_delivery.max_range = data.raw["ammo"]["explosive-cannon-shell"].ammo_type.action.action_delivery.max_range
+data.raw["ammo"]["uranium-cannon-shell-magazine"].ammo_type.action.action_delivery.max_range = data.raw["ammo"]["uranium-cannon-shell"].ammo_type.action.action_delivery.max_range
+data.raw["ammo"]["explosive-uranium-cannon-shell-magazine"].ammo_type.action.action_delivery.max_range = data.raw["ammo"]["explosive-uranium-cannon-shell"].ammo_type.action.action_delivery.max_range
